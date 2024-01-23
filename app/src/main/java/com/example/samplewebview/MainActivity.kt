69f75ab1ea87c51863b38b98a47fc9f7f6a26182
YasufumiT <tnkysfm.278323@icloud.com>
2024-01-23 23:07:27 +0900

    カメラ起動処理追加

--- app/src/main/java/com/example/samplewebview/MainActivity.kt
+++ app/src/main/java/com/example/samplewebview/MainActivity.kt
@@ -1,22 +1,65 @@
 package com.example.samplewebview
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.webkit.WebResourceRequest
 import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var sampleWebView: WebView
+//    private val REQUEST_IMAGE_CAPTURE = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // activity_main.xmlのWebViewに記載したidを指定
-        val webView: WebView = findViewById(R.id.webViewTest)
+        sampleWebView = findViewById(R.id.webViewTest)
 
         // WebViewClientを設定
         val customWebViewClient = SampleWebViewClient(this)
-        webView.webViewClient = customWebViewClient
+        sampleWebView.webViewClient = customWebViewClient
 
+        // Activity内で定義したWebViewClientを設定する場合
+        // setWebViewClient()
+
+        // JavaScript有効化
+        sampleWebView.getSettings().setJavaScriptEnabled(true);
         // assets配下に設置したHTMLの読み込み
-        webView.loadUrl("file:///android_asset/sample_html_view.html")
+        sampleWebView.loadUrl("file:///android_asset/sample_html_view.html")
     }
+
+    // Activity内でWebViewClientを定義
+//    private fun setWebViewClient() {
+//        sampleWebView.webViewClient = object: WebViewClient(){
+//            override fun shouldOverrideUrlLoading(
+//                view: WebView?,
+//                request: WebResourceRequest?
+//            ): Boolean {
+//                val url = request?.url.toString()
+//                // URLスキームを検知して、カメラを起動
+//                if (url == "event://start_scheme/") {
+//                    Log.d("MyApp", "スキーム発火");
+//                    dispatchTakePictureIntent()
+//                    return true
+//                }
+//                return false
+//            }
+//        }
+//    }
+
+//    private fun dispatchTakePictureIntent() {
+//        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+//        try {
+//            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+//        } catch (e: ActivityNotFoundException) {
+//            // display error state to the user
+//        }
+//    }
 }
--- app/src/main/java/com/example/samplewebview/SampleWebViewClient.kt
+++ app/src/main/java/com/example/samplewebview/SampleWebViewClient.kt
@@ -2,13 +2,19 @@ package com.example.samplewebview
 
 import android.app.Activity
 import android.app.AlertDialog
+import android.content.ActivityNotFoundException
 import android.content.Context
+import android.content.Intent
+import android.provider.MediaStore
 import android.util.Log
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.core.app.ActivityCompat.startActivityForResult
 
 class SampleWebViewClient(private val activity: Activity) : WebViewClient() {
+
+    private val REQUEST_IMAGE_CAPTURE = 1
 
     override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
         val url = request.url.toString()
@@ -17,9 +23,9 @@ class SampleWebViewClient(private val activity: Activity) : WebViewClient() {
         if (url == "event://start_scheme/") {
             Log.d("MyApp", "スキーム発火");
             showDialog(activity)
+            dispatchTakePictureIntent(activity)
             return true
         }
-
         return false
     }
 
@@ -32,5 +38,14 @@ class SampleWebViewClient(private val activity: Activity) : WebViewClient() {
                 dialog.dismiss()
             }
             .show()
+    }
+
+    private fun dispatchTakePictureIntent(activity: Activity) {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            startActivityForResult(activity,takePictureIntent, REQUEST_IMAGE_CAPTURE, null)
+        } catch (e: ActivityNotFoundException) {
+            Log.d("Log", e.toString())
+        }
     }
 }
